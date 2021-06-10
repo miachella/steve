ba048fca1e465fbfc5af1500925520790d8632c7
miachell <mathilde.iachella@gmail.com>
2021-06-10 14:49:28 +0200

    commit number one

---
+++ src/main/java/com/diginamic/coursintegration/CoursIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.diginamic.coursintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diginamic/coursintegration/CoursIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.coursintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
